d27095b20f8e2e08731a9efdbad83eec2a48d4fb
ArtoriaKnight <jacky88434591@hotmail.com>
2019-06-23 17:29:21 +0800

    no message

--- src/main/java/com/weixin/demo/DemoApplication.java
+++ src/main/java/com/weixin/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.weixin.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DemoApplication {
 
     public static void main(String[] args) {
